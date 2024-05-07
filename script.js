12b00ea7aac179d87723369f77ccdbe8469f3332
NoeliaMV <noeliaanahi.viera@gmail.com>
2024-05-06 23:51:50 -0300

    colores

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ function toggleMenu() {
 }
 
 
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
 
 
 let slideIndex = 1;
