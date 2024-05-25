edb4fb9617dc54280677edab7be8b3bb562d800d
NoeliaMV <noeliaanahi.viera@gmail.com>
2024-05-25 12:33:40 -0300

    Nuevo js para el menu

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-    const closeBtn = document.querySelector('.close-btn');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        hamburger.classList.toggle('active');
-        closeBtn.classList.toggle('active');
-    });
-
-    closeBtn.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        hamburger.classList.toggle('active');
-        closeBtn.classList.toggle('active');
-    });
-
-    // Cerrar el menú cuando se hace clic en un enlace
-    const navLinksList = document.querySelectorAll('.nav-links li a');
-    navLinksList.forEach(link => {
-        link.addEventListener('click', () => {
-            navLinks.classList.remove('active');
-            hamburger.classList.remove('active');
-            closeBtn.classList.remove('active');
-        });
-    });
-});
-
-
 window.addEventListener('scroll', function() {
     const header = document.getElementById('main-header');
     if (window.scrollY > 0) {
